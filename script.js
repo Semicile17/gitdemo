c8c5f5e0b409f8728dac79ebc349073c783b9c80
Semicile <Rohitsinghmahant707@gmail.com>
2023-09-20 09:47:24 +0530

    added all updated files

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("working");
+forward=document.querySelector('.forward');
+backward=document.querySelector('.backward');
+img=document.getElementById('img');
+let index=1;
+forward.addEventListener('click',()=>{
+  
+      if(index>=5){
+            index=1;
+      }
+      else{
+            index=index+1;
+      }
+      img.src=`img${index}.png`
+      console.log("working")
+})
+backward.addEventListener('click',()=>{
+      if(index<=1){
+            index=1;
+      }
+      else{
+            index=index-1;
+      }   
+      img.src=`img${index}.png`
+})
+
+
+     
